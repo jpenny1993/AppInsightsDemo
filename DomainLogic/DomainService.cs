2abdbae62b6163fa1e13e0d1c1e43c4587f59c89
John Penny <john.penny@ligentia.global>
2022-09-22 17:16:00 +0100

    Initial

---
+++ DomainLogic/DomainService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace DomainLogic;
+
+public class DomainService
+{
+    private readonly ILogger<DomainService> _logger;
+
+    public DomainService(ILogger<DomainService> logger)
+    {
+        _logger = logger;
+    }
+
+    public void DoWork()
+    {
+        _logger.LogInformation("Some amount of work has been done");
+    }
+}
